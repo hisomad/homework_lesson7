95d5a9ec3f7e2d2f1bfd3274cdf4c1d3b195c29c
Vika <vika.tulnikova@bk.ru>
2023-01-29 21:20:56 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿int m = 3;
+int n = 4;
+double[,] numbers = new double[m, n];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
